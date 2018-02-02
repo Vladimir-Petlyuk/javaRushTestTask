c86d6ff3136befafa484e51a58929385d2cf9916
Vladimir <petlyuk.vova@gmail.com>
2018-02-02 22:13:38 +0200

    Change

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -2,13 +2,12 @@ package ru;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.model.BookDto;
 import ru.service.BookService;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         BookService service = context.getBean(BookService.class);
-        for (int i = 0; i < 21; i++)
-            service.createBook("Вий", "Книга про зло", "918563247", "Николай Гоголь", 1850);
     }
 }
